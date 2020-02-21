2773684f543176f25c104879f6f588af2cdcc751
Mo Russ <mauriceruss302@gmail.com>
2020-02-21 14:20:10 -0500

    dice test one

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,30 @@
+import java.util.Random;
+
 public class Dice {
+    private int diceCraps;
+    private int diceYatz;
+    private Random rgen;
+    private int numberOfTosses;
 
 
+    public Dice(int numberOfTosses){
+        this.numberOfTosses = numberOfTosses;
+        rgen = new Random();
+
+    }
+    public Integer toss(){
+        int c =0;
+        for (int i = 0; i < numberOfTosses; i++) {
+            c += (rgen.nextInt(6) + 1);
+        }
+        return c;
+    }
+    public Dice(int numberOfTosses, long seed){
+        this.numberOfTosses = numberOfTosses;
+        rgen = new Random(seed);
+
+    }
+    public void setSeed(){
+        this.rgen.setSeed(15);
+    }
 }
---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+
+import java.util.Random;
+
+public class DiceTest {
+
+
+    @org.junit.Test
+    public void toss() {
+        //given
+        Dice dice1 = new Dice(2);
+        dice1.setSeed();
+        //when
+        int result = dice1.toss();
+        System.out.println(result);
+
+        result = dice1.toss();
+        System.out.println(result);
+
+        result = dice1.toss();
+        System.out.println(result);
+        //then
+        Assert.assertEquals(result, 5);
+    }
+}
